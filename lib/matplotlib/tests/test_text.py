319143d293a16128f1fa21c5dd7761a1d0142f1b
Samson Ma <samson.ma@mail.utoronto.ca>
2015-04-20 15:01:31 -0400

    Update image and test

--- lib/matplotlib/tests/test_text.py
+++ lib/matplotlib/tests/test_text.py
@@ -469,14 +469,11 @@ def test_font_wrap():
     t = "This is a really long string that I'd rather have wrapped so that" \
         " it doesn't go outside of the figure, but if it's long enough it" \
         " will go off the top or bottom!"
-    plt.text(4, -1, t, fontsize=18, family='serif', ha='left',
-             rotation=15, wrap=True)
-    plt.text(6, 5, t, family='sans serif', ha='left', rotation=15,
+    plt.text(4, -1, t, fontsize=18, family='serif', ha='left', rotation=15,
              wrap=True)
-    plt.text(5, 5, t, weight='light', ha='right',
-             rotation=-15, wrap=True)
-    plt.text(5, 10, t, family='fantasy', fontsize=18,
-             ha='center', va='top', wrap=True)
+    plt.text(6, 5, t, family='sans serif', ha='left', rotation=15, wrap=True)
+    plt.text(5, 5, t, weight='light', ha='right', rotation=-15, wrap=True)
+    plt.text(5, 10, t, weight='heavy', ha='center', va='top', wrap=True)
     plt.text(3, 4, t, family='monospace', ha='right', wrap=True)
-    plt.text(-1, 0, t, fontsize=14, style='italic', ha='left',
-             rotation=-15, wrap=True)
+    plt.text(-1, 0, t, fontsize=14, style='italic', ha='left', rotation=-15,
+             wrap=True)

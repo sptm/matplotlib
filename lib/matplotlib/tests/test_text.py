67022c1d47bf397e517444bba2220840566f8ced
Samson Ma <samson.ma@mail.utoronto.ca>
2015-04-20 13:59:57 -0400

    Testing fonts packaged in matplotlib

--- lib/matplotlib/tests/test_text.py
+++ lib/matplotlib/tests/test_text.py
@@ -469,13 +469,14 @@ def test_font_wrap():
     t = "This is a really long string that I'd rather have wrapped so that" \
         " it doesn't go outside of the figure, but if it's long enough it" \
         " will go off the top or bottom!"
-    plt.text(4, -1, t, fontsize=18, family='Umpush', ha='left', rotation=15,
+    plt.text(4, -1, t, fontsize=18, family='Bitstream Vera Serif', ha='left',
+             rotation=15, wrap=True)
+    plt.text(6, 5, t, family='Bitstream Vera Sans', ha='left', rotation=15,
              wrap=True)
-    plt.text(6, 5, t, family='Times New Roman', ha='left', rotation=15,
-             wrap=True)
-    plt.text(5, 5, t, family='monospace', ha='right', rotation=-15, wrap=True)
-    plt.text(5, 10, t, family='Waree', fontsize=18, style='oblique',
-             ha='center', va='top', wrap=True)
-    plt.text(3, 4, t, family='serif', style='italic', ha='right', wrap=True)
-    plt.text(-1, 0, t, fontsize=14, family='sans-serif', ha='left',
+    plt.text(5, 5, t, family='Bitstream Vera Sans Mono', ha='right',
              rotation=-15, wrap=True)
+    plt.text(5, 10, t, family='Bitstream Vera Serif', fontsize=18,
+             ha='center', va='top', wrap=True)
+    #plt.text(3, 4, t, family='serif', style='italic', ha='right', wrap=True)
+    #plt.text(-1, 0, t, fontsize=14, family='sans-serif', ha='left',
+    #        rotation=-15, wrap=True)
